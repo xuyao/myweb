c96e8b5b2f8af5f5559fd45675b3a302a568c3d9
xuyao <58394322@qq.com>
2015-10-09 23:11:34 +0800

    code

---
+++ wyper/src/main/java/com/wyper/App.java
@@ -0,0 +1,18 @@
+package com.wyper;
+
+/**
+ * 抓取网页解析
+ * 文章和相关图片按照时间的顺序保存
+ *
+ */
+public class App {
+    public static void main( String[] args ){
+    	//1、抓取单个网页参数
+    	
+    	
+    	//1、定时抓取
+    	
+    	
+        System.out.println( "Hello World!" );
+    }
+}
